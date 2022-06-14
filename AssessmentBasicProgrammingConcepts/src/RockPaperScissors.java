8745a5c7a429c0a076b94bbe13532967501629a7
Alicia Fisher <aliciajadefisher@yahoo.co.uk>
2022-06-14 14:46:43 +0100

    02 - Get round imput from user

--- AssessmentBasicProgrammingConcepts/src/RockPaperScissors.java
+++ AssessmentBasicProgrammingConcepts/src/RockPaperScissors.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class RockPaperScissors
 {
     public static void main(String[] args)
     {
+        int rounds;
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("======== Rock, Paper, Scissors ========");
+        System.out.print("How many rounds would you like to play? (1-10): ");
+
+        while(true)
+        {
+            rounds = Integer.parseInt(input.nextLine());
+
+            if(rounds < 1 || rounds > 10)
+            {
+                break;
+            }
+            else
+            {
+                System.out.println("Rounds: " + rounds);
+            }
+        }
     }
 }
